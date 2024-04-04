4de3369802fc3535997b6a20ac3ac367381cd700
zrefalogia <vuongquocphongv@gmail.com>
2024-04-04 10:39:47 +0700

    add reference to project

--- client/game/ScoreboardPanelScript/ScoreboardPanel.cs
+++ client/game/ScoreboardPanelScript/ScoreboardPanel.cs
@@ -1,18 +1,16 @@
 using Godot;
 using System;
 
-public partial class scoreboard_panel : Panel
+
+public partial class ScoreboardPanel : Panel
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// Hide the panel
-		this.Visible = true;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		// If game state is end game, show the panel overlay the current scene
 	}
 }
