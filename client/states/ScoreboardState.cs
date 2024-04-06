73bb2cf994624862bf38d021f0f8bc682fea07d2
zrefalogia <vuongquocphongv@gmail.com>
2024-04-06 13:41:15 +0700

    remove scoreboard state

--- client/states/ScoreboardState.cs
+++
@@ -1,7 +0,0 @@
-namespace StateManager {
-    public class ScoreboardState : IState {
-        public void Handle(GameComponents.GameManager gameManager) {
-            gameManager.TransitionTo(new PlayingState());
-        }
-    }
-}
